4eafa2f95e61ec5080bea121641c7f4bf8150c59
Adrien Gregorj <adrien.gregorj@gmail.com>
2018-06-10 17:15:41 +0900

    add first file

---
+++ polygons.py
@@ -0,0 +1,56 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+import argparse
+from colour import Color
+
+def compute_regular_polygon(n, size):
+    "Compute a regular polygon of the given shape"
+    poly = []
+    for k in range(n):
+        x = math.sqrt(size) * math.cos(2 * k * math.pi / n)
+        y = math.sqrt(size) * math.sin(2 * k * math.pi / n)
+        P = [x, y]
+        poly += [P]
+    return np.array(poly)
+
+def draw_shapes(shapes, colors):
+    "Draw the given shape"
+    for i in range(len(shapes)):
+        shape = shapes[i]
+        x = [c[0] for c in shape] + [shape[0][0]]
+        y = [c[1] for c in shape] + [shape[0][1]]
+        plt.plot(x, y, c=colors[i].rgb)
+        plt.axes().set_aspect('equal', 'datalim')
+    plt.show()
+
+def compute_inside_polygon(poly, r):
+    "Compute the polygon inside a given polygon with a rotation ratio of r"
+    n = len(poly)
+    inside_poly = []
+    for i in range(n):
+        P = r * poly[i] + (1 - r) * poly[(i+1)%n]
+        inside_poly += [P]
+    return np.array(inside_poly)
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-s", "--poly_size", help="size of the polygon", type=int, default=5)
+parser.add_argument("-n", "--iterations", help="number of iterations", type=int, default=10)
+parser.add_argument("-r", "--ratio", help="rotation ratio", type=float, default=0.2)
+args = parser.parse_args()
+
+
+c1 = Color("#000000")
+c2 = Color("#33cccc")
+colors = list(c1.range_to(c2, args.iterations + 1))
+
+init_poly = compute_regular_polygon(args.poly_size, 10)
+shapes =  [init_poly]
+
+inside = init_poly
+for i in range(args.iterations):
+    inside = compute_inside_polygon(inside, args.ratio)
+    shapes += [inside]
+
+draw_shapes(shapes, colors)
